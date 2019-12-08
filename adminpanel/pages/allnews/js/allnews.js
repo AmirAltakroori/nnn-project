5b791b90855d92e496312fe797f3ecbe025b7c38
farisabu3ram <aburamf@gmail.com>
2019-12-08 18:38:01 +0200

    Create the delete new functions and design the confirm window if the user want to delete the new.

--- adminpanel/pages/allnews/js/allnews.js
+++ adminpanel/pages/allnews/js/allnews.js
@@ -1,12 +1,39 @@
+let news = new Array();
 
+var newContain;
+var newId;
+function show(e,id) {
+    let f = document.getElementById('delete')
+    f.className += " active";
+    newContain = e;
+    newId=id;
+}
 
+function hide() {
+    let f = document.getElementById('delete')
+    f.classList.remove("active");
 
+}
 
+function deleteRowElement() {
 
+    let row = news.findIndex((row) => row.id == newId);
+    news.splice(row, 1);
+    var i = newContain.parentNode.parentNode;
+    i.parentElement.removeChild(i);
+    hide();
 
+}
+function deleteNews(callback, key, rev, row) {
 
-
-
-
-
-
+    let fullUrl = URL + "news/" + key + "?rev=" + rev;
+    let http = new XMLHttpRequest();
+    http.open("DELETE", fullUrl);
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    http.onreadystatechange = () => {
+        if (http.readyState == 4) {
+            callback(JSON.parse(http.response));
+            row.parentElement.removeChild(row);
+        }
+    }
+}
