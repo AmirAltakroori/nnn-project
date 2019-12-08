3033bddb83a01346ae168266d594147ea27a4945
Waleed Jubeh <wsameer70@gmail.com>
2019-12-09 02:47:57 +0300

    use pre-implemented function used in alluser page and set them up

--- adminpanel/pages/all_users/js/alluser.js
+++ adminpanel/pages/all_users/js/alluser.js
@@ -1,3 +1,57 @@
+let users = [{
+    firstName: "وليد",
+    lastName: "الجعبة",
+    email: "Waleed@ppu.edu",//We want also to show the email
+    isActive: 1,
+    roleId: 1,// when we create a user its diffault role is writer which its id is 1
+    id:1,
+}, {
+    firstName: "باسل",
+    lastName: "العطاونة",
+    email: "Basil@ppu.edu",//We want also to show the email
+    isActive: 1,
+    roleId: 2,// 2 its admin 
+    id:2,
+},
+{
+    firstName: "ديما",
+    lastName: "الحافظ",
+    email: "deema@ppu.edu",//We want also to show the email
+    isActive: 1,
+    roleId: 3,// 2 its admin 
+    id:3,
+},{
+    firstName: "شيماء",
+    lastName: "وزوز",
+    email: "shaima@ppu.edu",//We want also to show the email
+    isActive: 0,// its not active account.
+    roleId: 1,// 2 its writer. 
+    id:4,
+}];
+
+let newContain,newId=0;
+function show(row,modelId, id) {
+    let element = document.getElementById(modelId)
+    element.className += " modal-active";
+    newContain = row;
+    newId = id;
+}
+
+function hide(modelId) {
+    let element = document.getElementById(modelId)
+    element.classList.remove("modal-active");
+
+}
+
+function deleteRowElement() {
+
+    let row = users.findIndex((row) => row.id == newId);
+    users.splice(row, 1);
+    var i = newContain.parentNode.parentNode;
+    i.parentElement.removeChild(i);
+
+
+}
 
 function searchByUserName() {
     let input, filter, rows, tr, name, i, txtValue;
