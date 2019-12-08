898bc6f594eb8b0ca34e89d7fdd304a35cf9f6ed
Afnan Madhoun <afnanamdhoun@gmail.com>
2019-12-08 14:00:16 +0200

    Create a username search function for alluser page

---
+++ adminpanel/pages/all_users/js/alluser.js
@@ -0,0 +1,18 @@
+
+function searchByUserName() {
+    let input, filter, rows, tr, name, i, txtValue;
+    input = document.getElementById("userinput");
+    filter = input.value.toUpperCase();
+    rows = document.getElementById("rows");
+    tr = rows.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++)
+     {
+        name = tr[i].getElementsByTagName("td")[1];
+        txtValue = name.textContent || name.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";// to hide the 
+        }
+    }
+}
