e62b227119d0dbea12bea3593bcb105b2a3b75c4
majd ewawi <majd199ziad@gmail.com>
2019-12-09 13:19:06 +0200

    get the refrence of each button

---
+++ adminpanel/pages/homepage/js/home.js
@@ -0,0 +1,14 @@
+let newUserButton = document.getElementById('new-user');
+let allUsersButton = document.getElementById('all-users');
+let allNewsButton = document.getElementById('all-news');
+let categoriesButton = document.getElementById('categories');
+let addNewButton = document.getElementById('add-new');
+let approveNewsButton = document.getElementById('approve-news');
+let myNewsButton = document.getElementById('my-news');
+
+
+document.addEventListener('load' , function(){
+
+});
+
+
