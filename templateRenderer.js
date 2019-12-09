a4cf4dd8df8a00540efa03771b5061a6fb505386
Yosuef Rayyan <y.real.02@gmail.com>
2019-12-09 14:05:50 +0200

    Ability to bind variables in the view

--- templateRenderer.js
+++ templateRenderer.js
@@ -1,3 +1,7 @@
+var $scope = {};
+var $routeParams = {};
+var $bindedVars = new Map();
+
 // classes
 // holds data required to a special atributes
 class AttrData
@@ -73,18 +77,49 @@ function specialTags(doc)
 }
 
 // executes {{exp}} using data of a model
-function replaceElement(attrString,model)
+function replaceElement(attrString, model)
 {
-    // executes the exp
-    function replacer(attrString)
-    {
-        attrString = attrString.replace('{','');
-        attrString = attrString.replace('}','');
-        attrString = attrString.replace('$',"this.");
-        return eval(attrString);
-    }
-
-    // binds the previous function to the model
-    let re = replacer.bind(model);
-    return re(attrString);
+    let value = attrString.replace('$',"$scope.");
+    value = eval(value);
+    if(!value) return model.replace("{{" + attrString + "}}", "");
+    let res = model.replace("{{" + attrString + "}}", value);
+    console.log(res);
+    return res;
 }
+
+// Collects all the binded variables {{var}} in an array with their elements
+function collectBindedVariables(doc) {
+    let regExp = /\{\{([^}^}]+)\}\}/g;
+    doc.childNodes.forEach(element => {
+        if(text = element.innerText) {
+            let matches = text.match(regExp);
+            matches.forEach(str => {
+                let variable = str.substring(2, str.length - 2).trim();
+                if(!$bindedVars.get(variable)) $bindedVars.set(variable, [{element: element, text: element.innerText}]);
+                else $bindedVars.set(variable, [...$bindedVars.get(variable), {element: element, text: element.innerText}]);
+            });
+        }
+    });
+}
+
+// Replace the binded variable with its real value in html
+function $apply(vars = []) {
+    $bindedVars.forEach(async (elements, variable) => {
+        await elements.forEach(elem => {
+            elem.element.innerText = elem.text;
+        });
+        await elements.forEach(elem => {
+            elem.element.innerText = replaceElement(variable, elem.element.innerText);
+        });
+    });
+}
+
+collectBindedVariables(document.getElementById("root"));
+
+$scope.header = "NNN";
+$scope.header2 = "NNN2";
+$scope.header3 = "NNN3";
+$scope.header4 = "NNN4";
+$scope.text = "yaw yaw";
+
+$apply();
