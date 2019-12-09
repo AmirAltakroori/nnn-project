6ed3093c3742ec0e81c71d0f54de99af3a4d10c3
shaimawaz <161009@ppu.edu.ps>
2019-12-09 21:59:34 +0200

    read news functionality added

--- adminpanel/pages/allnews/js/allnews.js
+++ adminpanel/pages/allnews/js/allnews.js
@@ -1,7 +1,29 @@
+let categories = [
+    {
+        id: 1,
+        name: "الألعاب",
+        isActive: 1,
+    },
+    {
+        id: 3,
+        name: "الرئيسية",
+        isActive: 1,
+    },
+    {
+        id: 2,
+        name: "الرياضة",
+        isActive: 0,//غير مفعل
+    },
+    {
+        id: 4,
+        name: "الفن",
+        isActive: 0,
+    }]
+
 let news = [{
-    title: "test is test ",
+    title: "النصيرات أكثر المناطق هطولاً",
     content: "<h1>This is my first news</h1>",
-    categoryId: 1,
+    categoryId: 3,
     seoTitle: "First news",
     seoTags: "{ 'tags':{['test','sport']} }",
     seoDescription: "This is my first news",
@@ -14,35 +36,66 @@ let news = [{
     id: 1,
 },
 {
-    title: "test is test ",
+    title: "الالعاب الاولمبية قريبا",
     content: "<h1>This is my first news</h1>",
-    categoryId: 1,
+    categoryId: 2,
     seoTitle: "First news",
     seoTags: "{ 'tags':{['test','sport']} }",
     seoDescription: "This is my first news",
     isActive: 1,
-    isMainNews: 0,
-    isUrgentNews: 1,
+    isMainNews: 1,
+    isUrgentNews: 2,
     createDate: new Date(),
     writerId: 1,
     _attachments: "",
     id: 2,
 },
 {
-    title: "test is test ",
+    title: "أخبار الفن والفنانين والنجوم والمشاهير",
+    content: "<h1>This is my first news</h1>",
+    categoryId: 4,
+    seoTitle: "First news",
+    seoTags: "{ 'tags':{['test','sport']} }",
+    seoDescription: "This is my first news",
+    isActive: 1,
+    isMainNews: 0,
+    isUrgentNews: 0,
+    createDate: new Date(),
+    writerId: 1,
+    _attachments: "",
+    id: 3,
+},
+{
+    title: "الاحلال يعتقل مقدسيا مسنا",
+    content: "<h1>This is my first news</h1>",
+    categoryId: 3,
+    seoTitle: "First news",
+    seoTags: "{ 'tags':{['test','sport']} }",
+    seoDescription: "This is my first news",
+    isActive: 1,
+    isMainNews: 1,
+    isUrgentNews: 1,
+    createDate: new Date(),
+    writerId: 1,
+    _attachments: "",
+    id: 3,
+},
+{
+    title: "سلسلة العاب LEft 4 Dead تعود من جديد",
     content: "<h1>This is my first news</h1>",
     categoryId: 1,
     seoTitle: "First news",
     seoTags: "{ 'tags':{['test','sport']} }",
     seoDescription: "This is my first news",
     isActive: 1,
-    isMainNews: 0,
+    isMainNews: 1,
     isUrgentNews: 1,
     createDate: new Date(),
     writerId: 1,
     _attachments: "",
     id: 3,
 }];
+
 let newContain;
 let newId;
 function show(row,modelId, id) {
@@ -108,3 +161,116 @@ function searchNewsByCategory() {
 
     }
 }
+
+/* -----------------------------------------------------------------------------------------------------------------------------------------------------*/
+//read news functions 
+
+function findcat(id) {
+    for (let j = 0; j < categories.length; j++) {
+        if (categories[j].id == id)
+            return categories[j].name;
+    }
+}
+
+function displaynews(news){
+    let table = document.getElementById("tablebody");
+    for (let i = 0; i < news.length; i++) {
+        let row = document.createElement("tr");
+        row.className = "user_info";
+        row.id = i;
+
+        let number = document.createElement("td");
+        number.className = "user_no";
+        number.textContent = i;
+
+        let info = document.createElement("td");
+        info.className = "user_full";
+        let info_text = document.createElement("span");
+        info_text.className = "user_name allnews-title-limit";
+        info_text.textContent = news[i].title;
+        info.appendChild(info_text);
+
+        let categoery = document.createElement("td");
+        categoery.className = "user_full";
+        let cat_text = document.createElement("span");
+        cat_text.className = "user_name";
+        cat_text.textContent = findcat(news[i].categoryId);
+        categoery.appendChild(cat_text);
+
+        let statu = document.createElement("td");
+
+        let main_div = document.createElement("div");
+        main_div.className = "allnews-inline-block";
+        let main_check = document.createElement("input");
+        main_check.type = "checkbox";
+        main_check.id = "main" + i;
+
+        if (news[i].isMainNews)
+            main_check.checked = true;
+
+        main_check.tabIndex = "9";
+        let main_label = document.createElement("label");
+        main_label.for = "main" + i;
+        main_label.textContent = "خبر رئيسي";
+
+        main_div.appendChild(main_check);
+        main_div.appendChild(main_label);
+
+        let urgent_div = document.createElement("div");
+        urgent_div.className = "allnews-inline-block";
+        let urgent_check = document.createElement("input");
+        urgent_check.type = "checkbox";
+        urgent_check.id = "urgent" + i;
+
+        if (news[i].isUrgentNews)
+            urgent_check.checked = true;
+
+        urgent_check.tabIndex = "9";
+        let urgent_label = document.createElement("label");
+        urgent_label.for = "urgent" + i;
+        urgent_label.textContent = "خبر عاجل";
+
+        urgent_div.appendChild(urgent_check);
+        urgent_div.appendChild(urgent_label);
+
+        statu.appendChild(main_div);
+        statu.appendChild(urgent_div);
+
+        let show_selection = document.createElement("td");
+        let select = document.createElement("select");
+        select.className = "selection";
+
+        let option1 = document.createElement("option");
+        option1.value = 1;
+        option1.textContent = "إظهار في الموقع";
+        let option2 = document.createElement("option");
+        option2.value = 0;
+        option2.textContent = "إخفاء من الموقع";
+
+        select.appendChild(option1);
+        select.appendChild(option2);
+        show_selection.appendChild(select);
+
+        let operations = document.createElement("td");
+        let delete_icon = document.createElement("i");
+        delete_icon.className = "fas fa-trash-alt delete_user";
+        delete_icon.setAttribute ('onclick',"show(this,'delete',"+i+")");
+
+        let edit_icon = document.createElement("i");
+        edit_icon.className = "far fa-edit icon color-blue";
+
+        operations.appendChild(delete_icon);
+        operations.appendChild(edit_icon);
+
+        row.appendChild(number);
+        row.appendChild(info);
+        row.appendChild(categoery);
+        row.appendChild(statu);
+        row.appendChild(show_selection);
+        row.appendChild(operations);
+
+        table.appendChild(row);
+    }
+}
+
+document.onload = displaynews(news);
