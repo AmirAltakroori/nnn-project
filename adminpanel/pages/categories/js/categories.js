d13970e142c091f6dff74f567c28cf9788958575
Deema Hafiz <hafezdema1999@gmail.com>
2019-12-09 12:27:31 +0200

    modify modal style and js code

--- adminpanel/pages/categories/js/categories.js
+++ adminpanel/pages/categories/js/categories.js
@@ -1,78 +1,70 @@
-//Get the modal that opens when click on "إضافة فئة"
-let modal = document.getElementsByClassName("createcategory-modal")[0];
 
-// Get the button that opens the modal
-let addbtn = document.getElementsByClassName("add-button")[0];
+document.addEventListener('DOMContentLoaded', function (e) {
 
-//Get close icon that close the modal
-let span = document.getElementsByClassName("close")[0];
+  //Get the modal that opens when click on "إضافة فئة"
+  let modal = document.getElementById("createcategory-modal");
+  //Get the button that opens the modal
+  let addBtn = document.getElementsByClassName("add-button")[0];
+  
+  //Get close icon that close the modal
+  let span = document.getElementById("close");
+  
+  //Get the create button that will craete a new category 
+  let createBtn = document.getElementById("createcategory-btn");
+  
+  //When the user clicks on "انشاء", update actegories tabel with new category
+  createBtn.onclick = function() {
+    hideModal(modal.id);
+    let tbody = document.getElementsByTagName('tbody')[0];
+    let categoryName = document.getElementById("categoryname").value;
+    let tr = document.createElement('tr');
+    let row = `
+    <tr class="user_info" style="width:100%; font-family:"Segoe UI"">
+      <td class="user_no" style="font-size:30px">1</td>
+      <td class="user_full">
+          <span class="user_name" style="font-size:18px">`+categoryName+`</span>
+      </td>
+      <td>
+          <select class="selection" style="font-size:18px; border:none; font-family:"Segoe UI"">
+              <option value="writer">فعالة</option>
+              <option value="admin"> غير فعال</option>
+          </select>
+      </td>
+      <td>
+          <i class="fas fa-trash-alt delete_user" style="font-size:20px; color:red; text-align:center; cursor:pointer"></i>
+      </td>
+    </tr>`
+    tr.innerHTML = row;
+    tbody.appendChild(tr);
+  
+  //add the entered category to db
+  //addCategory();
+  }
+  //When the user clicks "إضافة فئة" , show the modal 
+  addBtn.onclick = function() {
+    showModal(modal.id);
+  }
+  //When the user clicks close icon , close the modal
+  span.onclick = function() {
+    hideModal(modal.id);
+  }
+  //When the user clicks anywhere outside of the modal, close it 
+  window.onclick = function(event) {
+    if (event.target == modal ) {
+        hideModal(modal.id);
+    }
+  }
+  
+});
 
-//Get the create button that will craete a new category 
-let createbtn = document.getElementsByClassName("createcategory-btn")[0];
-
-//Get input category from the input field 
-let categoryName = document.getElementById("categoryname").value;
-
-// When the user clicks the "اضافة فئة" button, open the modal 
-addbtn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on "انشاء", update actegories tabel with new category, close the modal and clear the input field
-createbtn.onclick = function() {
-  modal.style.display = "none";
-  let tbody = document.getElementsByTagName('tbody')[0];
-  let categoryName = document.getElementById("categoryname").value;
-  let tr = document.createElement('tr');
-  let row = `
-  <tr class="user_info" style="width:100%; font-family:"Segoe UI"">
-    <td class="user_no" style="font-size:30px">1</td>
-    <td class="user_full">
-        <span class="user_name" style="font-size:18px">`+categoryName+`</span>
-    </td>
-    <td>
-        <select class="selection" style="font-size:18px; border:none; font-family:"Segoe UI"">
-            <option value="writer">فعالة</option>
-            <option value="admin"> غير فعال</option>
-        </select>
-    </td>
-    <td>
-        <i class="fas fa-trash-alt delete_user" style="font-size:20px; color:red; text-align:center; cursor:pointer"></i>
-    </td>
-  </tr>`
-document.getElementById("categoryname").value = '';
-tr.innerHTML = row;
-tbody.appendChild(tr);
-
-
-//add the entered category to db
-//addCategory();
-}
-
-// When the user clicks on close icon, close the modal and clear the input field
-span.onclick = function() {
-  modal.style.display = "none";
+function showModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
   document.getElementById("categoryname").value = '';
 }
 
-// When the user clicks anywhere outside of the modal, close it and clear the input field
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    document.getElementById("categoryname").value = '';
-  }
+function hideModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
 }
-
-
-
-
-
-
-//create category and store its information in the db 
-
-
-
-
 
 
 
