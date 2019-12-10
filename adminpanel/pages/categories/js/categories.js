78672eef024dbdd6cdbd2f71d6f462997827f12c
Waleed Jubeh <wsameer70@gmail.com>
2019-12-10 14:35:12 +0300

    Modify the style of modal and edit create function in js file

--- adminpanel/pages/categories/js/categories.js
+++ adminpanel/pages/categories/js/categories.js
@@ -1,30 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(e) {
 
-document.addEventListener('DOMContentLoaded', function (e) {
+    //Get the modal that opens when click on "إضافة فئة"
+    let modal = document.getElementById("createcategory-modal");
+    //Get the button that opens the modal
+    let addBtn = document.getElementsByClassName("add-button")[0];
 
-  //Get the modal that opens when click on "إضافة فئة"
-  let modal = document.getElementById("createcategory-modal");
-  //Get the button that opens the modal
-  let addBtn = document.getElementsByClassName("add-button")[0];
-  
-  //Get close icon that close the modal
-  let span = document.getElementById("close");
-  
-  //Get the create button that will craete a new category 
-  let createBtn = document.getElementById("createcategory-btn");
-  
-  //When the user clicks on "انشاء", update actegories tabel with new category
-  let addForm = document.getElementById("category-form");
-  addForm.addEventListener("submit", (e) => {
+    //Get close icon that close the modal
+    let span = document.getElementById("close");
 
-      
-      let tbody = document.getElementsByTagName('tbody')[0];
-      let categoryName = document.getElementById("categoryname").value;
-      let tr = document.createElement('tr');
-      let row = `
+    //Get the create button that will craete a new category 
+    let createBtn = document.getElementById("createcategory-btn");
+
+    //When the user clicks on "انشاء", update actegories tabel with new category
+    let addForm = document.getElementById("category-form");
+    addForm.addEventListener("submit", (e) => {
+
+
+        let tbody = document.getElementsByTagName('tbody')[0];
+        let categoryName = document.getElementById("categoryname").value;
+        let tr = document.createElement('tr');
+        let row = `
       <tr class="user_info" style="width:100%; font-family:"Segoe UI"">
-        <td class="user_no" style="font-size:30px">1</td>
+        <td class="user_no" style="font-size:30px">${tbody.childElementCount+1}</td>
         <td class="user_full">
-            <span class="user_name" style="font-size:18px">`+categoryName+`</span>
+            <span class="user_name" style="font-size:18px">` + categoryName + `</span>
         </td>
         <td>
             <select class="selection" style="font-size:18px; border:none; font-family:"Segoe UI"">
@@ -36,52 +35,40 @@ document.addEventListener('DOMContentLoaded', function (e) {
             <i class="fas fa-trash-alt delete_user" style="font-size:20px; color:red; text-align:center; cursor:pointer"></i>
             <i class="far fa-edit icon color-blue"></i>
         </td>
-      </tr>`
-      tr.innerHTML = row;
-      tbody.appendChild(tr);
-      hideModal(modal.id);
-      e.preventDefault();
-      return false;
-  });
- 
-  //When the user clicks "إضافة فئة" , show the modal 
-  addBtn.onclick = function() {
-    showModal(modal.id);
-  }
-  //When the user clicks close icon , close the modal
-  span.onclick = function() {
-    hideModal(modal.id);
-  }
-  //When the user clicks anywhere outside of the modal, close it 
-  window.onclick = function(event) {
-    if (event.target == modal ) {
+      </tr>`;
+        tr.innerHTML = row;
+        tbody.appendChild(tr);
         hideModal(modal.id);
+        // to database
+        e.preventDefault();
+        return false;
+    });
+
+    //When the user clicks "إضافة فئة" , show the modal 
+    addBtn.onclick = function() {
+            showModal(modal.id);
+        }
+        //When the user clicks close icon , close the modal
+    span.onclick = function() {
+            hideModal(modal.id);
+        }
+        //When the user clicks anywhere outside of the modal, close it 
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            hideModal(modal.id);
+        }
     }
-  }
-  
+
 });
 
 function showModal(modalId) {
-  document.getElementById(modalId).style.display = "block";
-  document.getElementById("categoryname").value = '';
+    document.getElementById(modalId).style.display = "flex";
+    document.getElementById("categoryname").value = '';
 }
 
 function hideModal(modalId) {
-  document.getElementById(modalId).style.display = "none";
+    document.getElementById(modalId).style.display = "none";
 }
-
-// check if the entered name for the category is empty and alert the user
-/*function ValidationEvent(){
-  if(document.getElementById("categoryname").value = ""){
-    alert("You must enter a name for the category");
-    return false;
-  }
-  return true;
-}*/
-
-
-
-
 /**
  * This function create to filter a categories when the user search
  */
@@ -101,8 +88,7 @@ const searchByCategory = () => {
 
             if (textValue.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
-            }
-            else {
+            } else {
                 tr[i].style.display = "none";
             }
         }
