d0b6b7994b8b9766fcc1128da346ffe1fdccc3ce
ibrahim123abusamrah <47473456+ibrahim123abusamrah@users.noreply.github.com>
2019-12-12 00:45:30 +0200

    make desgin and make funtion to show categeory

--- src/controller/categories.js
+++ src/controller/categories.js
@@ -1,9 +1,42 @@
 /*
-*     NNN website.
-*
-*     This file is part of the NNN website.
-*
-*     Authors:
-*
-*     File description:
+ *     NNN website.
+ *
+ *     This file is part of the NNN website.
+ *
+ *     Authors: ibrahim abusamarah
+ *
+ *     File description: this file contains the functions that fetch Related news category from the database
+ */
+
+/*
+    Show Related category News.
+     
+    @tparam 
+     
+    @param 
+     
+    @returns 
+    
+    This function used to show  the Related category News retrieved from database 
 */
+
+function related_category_news() {
+    getRelated_category_news();
+    for (let i = 0; i < Object.keys(NewsInCategory).length; i++) {
+        if (whereAddNew % 2 == 0) {
+            var news_li = document.createElement("li");
+            news_li.setAttribute.Id = NewsInCategory.Id;
+            news_li.innerHTML = `
+             <p>${NewsInCategory.title}</p>
+             <img src="${NewsInCategory.img}" alt="photo">`
+            document.getElementById("related_news_menu_left").appendChild(news_li);
+        } else {
+            var news_li = document.createElement("li");
+            news_li.setAttribute.Id = NewsInCategory.Id;
+            news_li.innerHTML = `
+             <p>${NewsInCategory.title}</p>
+             <img src="${NewsInCategory.img}" alt="photo">`
+            document.getElementById("related_news_menu_right").appendChild(news_li);
+        }
+    }
+}
--- src/model/categories.js
+++ src/model/categories.js
@@ -1,9 +1,33 @@
 /*
-*     NNN website.
-*
-*     This file is part of the NNN website.
-*
-*     Authors:
-*
-*     File description:
+ *     NNN website.
+ *
+ *     This file is part of the NNN website.
+ *
+ *     Authors: ibrahim abusamarah
+ *
+ *     File description: this file contains the functions that fetch Related news category from the database
+ */
+
+/*
+    get data from database     
+    @tparam 
+     
+    @param 
+     
+    @returns 
+    
+    This function used to get the Related news category  retrieved from database 
 */
+function getRelated_category_news() {
+    NewsInCategory = [{
+            "Id": "1",
+            "title": "دعوات لقطع الطرقات في اليوم الـ40 لانطلاق5555555",
+            "img": "img/new.jpg"
+        },
+        {
+            "Id": "2",
+            "title": "دعوات لقطع الطرقات في اليوم الـ40 لانطلاق5555555",
+            "img": "img/new.jpg"
+        }
+    ];
+}
