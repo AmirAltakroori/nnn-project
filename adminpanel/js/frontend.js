080676f8f812818e445519c5ba510822ae57a591
majd ewawi <majd199ziad@gmail.com>
2020-01-09 23:10:33 +0200

    generalize search function for all pages update script source and delete repeated functions

--- adminpanel/js/frontend.js
+++ adminpanel/js/frontend.js
@@ -7,3 +7,32 @@
 // Function's names must be lowCamelCase 
 // Don't delete this comments
 // By Waleed Jubeh
+
+
+function searchElement(columnIndex) {
+    let searchInput, searchText, table, tableBody, tr, i, td, span, category;
+
+    // get text from search input
+    searchInput = document.getElementById('search');
+    searchText = searchInput.value;
+
+    // get all elements in the table body 
+    table = document.getElementById('table');
+    tableBody = document.getElementsByTagName("tbody")[0];
+    tr = tableBody.getElementsByTagName('tr');
+
+    // traverse through each element in the table
+    for (i = 0; i < tr.length; i++) {
+        // get element searched for from each element in table
+        td = tr[i].getElementsByTagName('td')[columnIndex];
+        span = td.getElementsByTagName('span')[0];
+        serachedElement = span.childNodes[0].nodeValue;
+
+        //check if the element contain search text and filter the result
+        if (searchedElement.indexOf(searchText) > -1)
+            tr[i].style.display = "";
+        else
+            tr[i].style.display = "none";
+
+    }
+}
--- adminpanel/pages/categories/js/categories.js
+++ adminpanel/pages/categories/js/categories.js
@@ -110,33 +110,6 @@ function showModal(modalId) {
 function hideModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
-/**
- * This function create to filter a categories when the user search
- */
-
-
-
-const searchByCategory = () => {
-    let filter = document.getElementById('search').value.toUpperCase();
-    let content = document.getElementById('content');
-    let tr = content.getElementsByTagName('tr');
-
-    for (let i = 0; i < tr.length; i++) {
-        let td = tr[i].getElementsByTagName('td')[1];
-
-        if (td) {
-            let textValue = td.textContent || td.innerHTML;
-
-            if (textValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-
-
-    }
-}
 
 let activeId = 0;
 let activeRow = null;
