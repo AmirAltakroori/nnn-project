5533897dbc02327d3e1cf52203eb775cac798d32
basil.atawneh <161073@ppu.edu.ps>
2020-01-11 12:37:07 +0200

    generalize news data and update function

--- adminpanel/js/backend.js
+++ adminpanel/js/backend.js
@@ -12,3 +12,98 @@ function getData(storeName) {
 function saveData(storeName, data) {
     sessionStorage.setItem(storeName, JSON.stringify(data));
 }
+let newsPage = [{
+    id: 1,
+    title: "test is test ",
+    content: "<h1>This is my first news</h1>",
+    categoryId: 1,
+    seoTitle: "First news",
+    seoTags: "{ 'tags':{['test','sport']} }",
+    seoDescription: "This is my first news",
+    isActive: 1,
+    isMainNews: 0,
+    isUrgentNews: 1,
+    createDate: new Date(),
+    writerId: 1,
+    _attachments: "",
+},
+{
+    id: 2,
+    title: "test is test ",
+    content: "<h1>This is my first news</h1>",
+    categoryId: 1,
+    seoTitle: "First news",
+    seoTags: "{ 'tags':{['test','sport']} }",
+    seoDescription: "This is my first news",
+    isActive: 1,
+    isMainNews: 0,
+    isUrgentNews: 1,
+    createDate: new Date(),
+    writerId: 1,
+    _attachments: "",
+},
+{
+    id: 3,
+    title: "test is test ",
+    content: "<h1>This is my first news</h1><br><ul><li>ههههه</li></ul>",
+    categoryId: 1,
+    seoTitle: "First news",
+    seoTags: "شسيشس شسيشس شسي شسي",
+    seoDescription: "This is my first news",
+    isActive: 1,
+    isMainNews: 0,
+    isUrgentNews: 1,
+    createDate: "2019-05-12",
+    writerId: 1,
+    _attachments: "",
+},
+{
+    title: "الاحلال يعتقل مقدسيا مسنا",
+    content: "<h1>This is my first news</h1>",
+    categoryId: 3,
+    seoTitle: "First news",
+    seoTags: "{ 'tags':{['test','sport']} }",
+    seoDescription: "This is my first news",
+    isActive: 0,
+    isMainNews: 1,
+    isUrgentNews: 1,
+    createDate: "2019-05-12",
+    writerId: 1,
+    _attachments: "",
+    id: 4,
+},
+
+];
+let categories = [{
+    id: 1,
+    name: "الألعاب",
+    isActive: 1,
+},
+{
+    id: 3,
+    name: "الرئيسية",
+    isActive: 1,
+},
+{
+    id: 2,
+    name: "الرياضة",
+    isActive: 0, //غير مفعل
+},
+{
+    id: 4,
+    name: "الفن",
+    isActive: 0,
+}
+]
+
+function updateNews(id) {
+    let aim = null;
+    for (ind in newsPage)
+        if (newsPage[ind].id == id) {
+            aim = newsPage[ind];
+            aim["ind"] = ind;
+            break;
+        }
+    sessionStorage.setItem("userData", JSON.stringify(aim));
+    window.location.href = "../addnewspage/addnewpage.html";
+}
--- adminpanel/pages/mynews/js/mynews.js
+++ adminpanel/pages/mynews/js/mynews.js
@@ -1,86 +1,4 @@
-let newsPage = [{
-        id: 1,
-        title: "test is test ",
-        content: "<h1>This is my first news</h1>",
-        categoryId: 1,
-        seoTitle: "First news",
-        seoTags: "{ 'tags':{['test','sport']} }",
-        seoDescription: "This is my first news",
-        isActive: 1,
-        isMainNews: 0,
-        isUrgentNews: 1,
-        createDate: new Date(),
-        writerId: 1,
-        _attachments: "",
-    },
-    {
-        id: 2,
-        title: "test is test ",
-        content: "<h1>This is my first news</h1>",
-        categoryId: 1,
-        seoTitle: "First news",
-        seoTags: "{ 'tags':{['test','sport']} }",
-        seoDescription: "This is my first news",
-        isActive: 1,
-        isMainNews: 0,
-        isUrgentNews: 1,
-        createDate: new Date(),
-        writerId: 1,
-        _attachments: "",
-    },
-    {
-        id: 3,
-        title: "test is test ",
-        content: "<h1>This is my first news</h1><br><ul><li>ههههه</li></ul>",
-        categoryId: 1,
-        seoTitle: "First news",
-        seoTags: "شسيشس شسيشس شسي شسي",
-        seoDescription: "This is my first news",
-        isActive: 1,
-        isMainNews: 0,
-        isUrgentNews: 1,
-        createDate: "2019-05-12",
-        writerId: 1,
-        _attachments: "",
-    },
-    {
-        title: "الاحلال يعتقل مقدسيا مسنا",
-        content: "<h1>This is my first news</h1>",
-        categoryId: 3,
-        seoTitle: "First news",
-        seoTags: "{ 'tags':{['test','sport']} }",
-        seoDescription: "This is my first news",
-        isActive: 0,
-        isMainNews: 1,
-        isUrgentNews: 1,
-        createDate: "2019-05-12",
-        writerId: 1,
-        _attachments: "",
-        id: 4,
-    },
 
-];
-let categories = [{
-        id: 1,
-        name: "الألعاب",
-        isActive: 1,
-    },
-    {
-        id: 3,
-        name: "الرئيسية",
-        isActive: 1,
-    },
-    {
-        id: 2,
-        name: "الرياضة",
-        isActive: 0, //غير مفعل
-    },
-    {
-        id: 4,
-        name: "الفن",
-        isActive: 0,
-    }
-]
 document.addEventListener("DOMContentLoaded", (event) => {
     let userdata = JSON.parse(sessionStorage.getItem("userData"));
     if (userdata != null) {
@@ -90,17 +8,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 
-function updateNews(id) {
-    let aim = null;
-    for (ind in newsPage)
-        if (newsPage[ind].id == id) {
-            aim = newsPage[ind];
-            aim["ind"] = ind;
-            break;
-        }
-    sessionStorage.setItem("userData", JSON.stringify(aim));
-    window.location.href = "../addnewspage/addnewpage.html";
-}
 
 function deleteNews(callback, key, rev, row) {
 
