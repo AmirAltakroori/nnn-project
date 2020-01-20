7d8a74550fe80a7f0db1153cc57a163746f36ded
Suhair Shareef <suhairshareef1999@gmail.com>
2020-01-20 13:33:37 +0200

    Categories section JS

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,13 @@
      Authors:
      Aseel Arafeh <arafehaseel@gmail.com> ,
      Rahmeh Tartouri <Rahmeh.Tartouri@gmail.com>
+     Suhair Shareef <suhairshareef1999@gmail.com>
 
      File description:
 */
 
 
-const urgentNewsDiv = document.getElementById('urgent-content');
+let urgentNewsDiv = document.getElementById('urgent-content').innerHTML;
 let urgentNews = [];
 
 showUrgentNews();
@@ -58,7 +59,7 @@ function showUrgentNews () {
 
     getUrgentNews();
     
-    urgentNewsDiv.innerHTML="";
+    urgentNewsDiv=null;
     for (let i = 0; i < urgentNews.length; i++) {
 
         let currentUrgentNew = `<a href="${urgentNews[i].path}">${urgentNews[i].title} </a>`;
@@ -72,11 +73,13 @@ function showUrgentNews () {
 
 // JS for Category Sectio in home Page 
 
-const categoryMainNewsDiv = document.getElementById('');
-const categorySubNewsDiv = document.getElementById('');
+const categoryTitleDiv = document.getElementById('title');
+const categoryMainNewsDiv = document.getElementById('main-news');
+const categorySubNewsDiv = document.getElementById('sub-news');
+let categoryTitle;
 let categoryMainNews = [];
 
-showCategoryMainNews();
+showCategoryNews();
 
 /*
     Get main News in Category
@@ -89,18 +92,23 @@ showCategoryMainNews();
     
     This function used to retrieve main news in category from database 
 */ 
-function getCategoryMainNews () {
+function getCategoryNews () {
 
-    // I'll rewrite this function when DB was ready.
+// I'll rewrite this function when DB was ready.
+    categoryTitle = [{
+                        title:"الأخبار العالمية",
+                        path:"#"
+                     }]
+
     categoryMainNews = [{
                         title: " القوة الخامسة للطبيعة.. اكتشاف قد يفك لغز المادة المظلمة",
                         path: "#",
                         authorName: "admin",
                         publishedDate: "12/12/2019",
-                        img:"news.jpg",
-                        SubDescription:"",
-                        description:""
+                        img:"src\view\img\img1.jpg",
+                        SubDescription:"فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم "
                        }];
+    
 
 }
 
@@ -115,38 +123,38 @@ function getCategoryMainNews () {
     
     This function used to show the urgent news retrieved from database at Category Section in home bage
 */
-function showCategoryMainNews () {
+function showCategoryNews () {
  
-    getCategoryMainNews();
-    categoryMainNewsDiv.innerHTML="";
+    getCategoryNews();
+    categoryTitleDiv.innerHTML=`<div class="category-header-name"><a class="link" href="${categoryTitle.title}"></a></div>
+                                <div class="category-read-more "><a class="read-more-btn" href=${categoryTitle.path}> المزيد &gt;</a></div>`;
+
+
+
     if(categoryMainNews.length > 0) {
  
-        let currentCategoryMainNewsPath = `<a href="${categoryMainNews[0].path}"></a>`;
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsPath;
-        let currentCategoryMainNewsTitle = `<a href="${categoryMainNews[0].path}">${categoryMainNews[0].title} </a>`; 
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsTitle;
-        let currentCategoryMainNewsAuther = `<p>${categoryMainNews[0].authorName} </p>`;
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsAuther;
-        let currentCategoryMainNewsPublishedDate = `<p>${categoryMainNews[0].publishedDate} </p>`;
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsPublishedDate;
-        let currentCategoryMainNewsImgage = `<img src="${categoryMainNews[0].img}"/>`; 
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsImgage;
-        let currentCategoryMainNewsDescription = `<p>${categoryMainNews[0].SubDescription} </p>`;
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsDescription;
- 
-    }
+        categoryMainNewsDiv.innerHTML=`<div class="category-main-image-div">
+                                            <img id="category-image" src="${categoryMainNews[0].img}" alt="${categoryMainNews[0].title}">
+                                        </div>
+                                        <div class="category-main-title-and-date">
+                                                <div class="category-main-title"><a class="link" href="${categoryMainNews[0].path}"></a></div>
+                                                <div class="category-main-date">${categoryMainNews[0].publishedDate}</div>
+                                        </div>
+                                        <div class="category-main-details"></div>
+                                        <a class="read-more-btn category-btn" href=${categoryMainNews[0].path}>اقرأ المزيد &gt;</a>`;
+
+    
     
     categorySubNewsDiv.innerHTML="";
-    for (let i = 1; i < categoryMainNews.length(); i++) {
+    
+    for (let i = 0; i < 3; i++) {
+        
+        categorySubNewsDiv.innerHTML+=`<div class="category-content-random-item">
+                                            <img class="category-img-rnd" id="category-image" src="${categoryMainNews[0].img}" alt="">
+                                            <div class="category-randome-title"><a class="link" href="${categoryMainNews[0].path}">${categoryMainNews[0].title}</a> </div>
+                                        </div>`;
  
-        let currentCategoryMainNewsTitle = `<a href="${categoryMainNews[i].path}">${categoryMainNews[0].title} </a>`; 
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsTitle;
-        let currentCategoryMainNewsPublishedDate = `<p>${categoryMainNews[i].publishedDate} </p>`;
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsPublishedDate;
-        let currentCategoryMainNewsImgage = `<img src="${categoryMainNews[i].img}"/>`; 
-        categoryMainNewsDiv.innerHTML += currentCategoryMainNewsImgage;
- 
- 
-    }    
+    } 
+}   
  
 }
