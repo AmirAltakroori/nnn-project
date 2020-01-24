9cea6e47a155907383f15faaa379aac295eb4836
Deema Hafiz <hafezdema1999@gmail.com>
2020-01-24 13:49:55 +0200

    partialy refactor display news usong potato framework

--- adminpanel/js/mynews.js
+++ adminpanel/js/mynews.js
@@ -1,107 +1,15 @@
-export class mynews {
 
-
-     myNewsPage = [{
-        id: 1,
-        title: "test is test ",
-        content: "<h1>This is my first news</h1>",
-        categoryId: 1,
-        seoTitle: "First news",
-        seoTags: "{ 'tags':{['test','sport']} }",
-        seoDescription: "This is my first news",
-        isActive: 1,
-        isMainNews: 0,
-        isUrgentNews: 1,
-        createDate: new Date(),
-        writerId: 1,
-        _attachments: "",
-    },
-    {
-        id: 2,
-        title: "test is test ",
-        content: "<h1>This is my first news</h1>",
-        categoryId: 1,
-        seoTitle: "First news",
-        seoTags: "{ 'tags':{['test','sport']} }",
-        seoDescription: "This is my first news",
-        isActive: 1,
-        isMainNews: 0,
-        isUrgentNews: 1,
-        createDate: new Date(),
-        writerId: 1,
-        _attachments: "",
-    },
-    {
-        id: 3,
-        title: "test is test ",
-        content: "<h1>This is my first news</h1><br><ul><li>ههههه</li></ul>",
-        categoryId: 1,
-        seoTitle: "First news",
-        seoTags: "شسيشس شسيشس شسي شسي",
-        seoDescription: "This is my first news",
-        isActive: 1,
-        isMainNews: 0,
-        isUrgentNews: 1,
-        createDate: "2019-05-12",
-        writerId: 1,
-        _attachments: "",
-    },
-    {
-        title: "الاحلال يعتقل مقدسيا مسنا",
-        content: "<h1>This is my first news</h1>",
-        categoryId: 3,
-        seoTitle: "First news",
-        seoTags: "{ 'tags':{['test','sport']} }",
-        seoDescription: "This is my first news",
-        isActive: 0,
-        isMainNews: 1,
-        isUrgentNews: 1,
-        createDate: "2019-05-12",
-        writerId: 1,
-        _attachments: "",
-        id: 4,
-    },
-
-];
- categories = [{
-        id: 1,
-        name: "الألعاب",
-        isActive: 1,
-    },
-    {
-        id: 3,
-        name: "الرئيسية",
-        isActive: 1,
-    },
-    {
-        id: 2,
-        name: "الرياضة",
-        isActive: 0, //غير مفعل
-    },
-    {
-        id: 4,
-        name: "الفن",
-        isActive: 0,
+document.addEventListener("DOMContentLoaded", (event) => {
+    let userdata = JSON.parse(sessionStorage.getItem("userData"));
+    if (userdata != null) {
+        myNewsPage[userdata.ind] = userdata;
+        sessionStorage.removeItem("userData");
     }
-];
+});
 
-constructor(){
 
-    document.addEventListener("DOMContentLoaded", (event) => {
-        let userdata = JSON.parse(sessionStorage.getItem("userData"));
-        if (userdata != null) {
-            myNewsPage[userdata.ind] = userdata;
-            sessionStorage.removeItem("userData");
-        }
-    });
 
-    document.addEventListener("DOMContentLoaded", (event) => {
-
-        //displaynews(myNewsPage);
-    });
-}
-
- deleteNews(callback, key, rev, row) {
+function deleteNews(callback, key, rev, row) {
 
     let fullUrl = URL + "news/" + key + "?rev=" + rev;
     let http = new XMLHttpRequest();
@@ -119,14 +27,14 @@ constructor(){
 //read news functions 
 
 
- findCat(id) {
+function findCat(id) {
     for (let j = 0; j < categories.length; j++) {
         if (categories[j].id == id)
             return categories[j].name;
     }
 }
 
- displaynews(news) {
+function displaynews(news) {
     newsPage = news;
     let table = document.getElementById("tablebody");
     for (let i = 0; i < news.length; i++) {
@@ -181,5 +89,7 @@ constructor(){
     }
 }
 
+document.addEventListener("DOMContentLoaded", (event) => {
 
-}
+    displaynews(myNewsPage);
+});
