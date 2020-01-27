ab4139020eb95ed822c2591b8de9e292dfb5508b
MasriLatifa <161040@ppu.edu.ps>
2020-01-27 15:08:41 +0200

    ..

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ import { Mvc } from './potato/dist/jsMvc.min.js';
 let mvc = new Mvc();
 
 let routeList = [{
-    url: "/details/:id",
-    template: "/templates/details.html",
-    controller: "/controllers/detailesClass.js",
-    id: "",
-    title: "new"
-}];
+        url: "/",
+        template: "/templates/index.html",
+        controller: "/controllers/index.js",
+        title: "Home"
+    },
+
+    {
+        url: "/details",
+        template: "/templates/details.html",
+        controller: "/controllers/detailesClass.js",
+        title: "new"
+    }
+];
 
 
 mvc.addRouteList(routeList);
