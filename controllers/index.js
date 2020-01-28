adb1839e40149c6f7636f7f16f80f205c486439f
SuhairShareef <suhairshareef1999@gmail.com>
2020-01-28 21:51:35 +0200

    Fix

--- controllers/index.js
+++ controllers/index.js
@@ -228,38 +228,38 @@ function getCategoryNews () {
 
     This function used to show the urgent news retrieved from database at Category Section in home bage
 */
-// function showCategoryNews () {
+function showCategoryNews () {
 
-//     getCategoryNews();
-//     categoryTitleDiv.innerHTML=`<div class="category-header-name"><a class="link" href="${categoryTitle.title}"></a></div>
-//                                 <div class="category-read-more "><a class="read-more-btn" href=${categoryTitle.path}> المزيد &gt;</a></div>`;
-
-
-
-//     if(categoryMainNews.length > 0) {
-
-//         categoryMainNewsDiv.innerHTML=`<div class="category-main-image-div">
-//                                             <img id="category-image" src="${categoryMainNews[0].img}" alt="${categoryMainNews[0].title}">
-//                                         </div>
-//                                         <div class="category-main-title-and-date">
-//                                                 <div class="category-main-title"><a class="link" href="${categoryMainNews[0].path}"></a></div>
-//                                                 <div class="category-main-date">${categoryMainNews[0].publishedDate}</div>
-//                                         </div>
-//                                         <div class="category-main-details"></div>
-//                                         <a class="read-more-btn category-btn" href=${categoryMainNews[0].path}>اقرأ المزيد &gt;</a>`;
+    getCategoryNews();
+    categoryTitleDiv.innerHTML=`<div class="category-header-name"><a class="link" href="${categoryTitle.title}"></a></div>
+                                <div class="category-read-more "><a class="read-more-btn" href=${categoryTitle.path}> المزيد &gt;</a></div>`;
 
 
 
-//     categorySubNewsDiv.innerHTML="";
+    if(categoryMainNews.length > 0) {
 
-//     for (let i = 0; i < 3; i++) {
+        categoryMainNewsDiv.innerHTML=`<div class="category-main-image-div">
+                                            <img id="category-image" src="${categoryMainNews[0].img}" alt="${categoryMainNews[0].title}">
+                                        </div>
+                                        <div class="category-main-title-and-date">
+                                                <div class="category-main-title"><a class="link" href="${categoryMainNews[0].path}"></a></div>
+                                                <div class="category-main-date">${categoryMainNews[0].publishedDate}</div>
+                                        </div>
+                                        <div class="category-main-details"></div>
+                                        <a class="read-more-btn category-btn" href=${categoryMainNews[0].path}>اقرأ المزيد &gt;</a>`;
 
-//         categorySubNewsDiv.innerHTML+=`<div class="category-content-random-item">
-//                                             <img class="category-img-rnd" id="category-image" src="${categoryMainNews[0].img}" alt="">
-//                                             <div class="category-randome-title"><a class="link" href="${categoryMainNews[0].path}">${categoryMainNews[0].title}</a> </div>
-//                                         </div>`;
 
-//     }
-// }
 
-// }
+    categorySubNewsDiv.innerHTML="";
+
+    for (let i = 0; i < 3; i++) {
+
+        categorySubNewsDiv.innerHTML+=`<div class="category-content-random-item">
+                                            <img class="category-img-rnd" id="category-image" src="${categoryMainNews[0].img}" alt="">
+                                            <div class="category-randome-title"><a class="link" href="${categoryMainNews[0].path}">${categoryMainNews[0].title}</a> </div>
+                                        </div>`;
+
+    }
+}
+
+}
