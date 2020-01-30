ee4d8f77f0c98ae1fbfd65a40df2befd1529addb
Waleed Jubeh <wsameer70@gmail.com>
2020-01-30 22:28:26 +0300

    change the site mode and test it

--- adminpanel/pages/homepage/js/home.js
+++ adminpanel/pages/homepage/js/home.js
@@ -1,4 +1,4 @@
-  export class HomeController {
+export class HomeController {
     constructor() {
         this.mode = null;
 
@@ -7,18 +7,45 @@
             window.location.href = '/admin-panel-login/login.html';
             return;
         }
+        this.changing = false;
         this.userRole = userData.roleID;
         console.log(this.userRole)
         this.db = dynamicImport("./js/backend.js");
         console.log(this.db);
-        this.db.then(dbObject => dbObject.dbGet("/settings", false, "sitemode").then(res => {
-            this.mode = res.state;
-            console.log(res);
+        this.siteMode = null;
+        this.getStatus().then(state => {
+            this.siteMode = state;
+            this.mode = state.state;
+            console.log(this.mode);
             mvc.apply();
-        }));
-
+        });
 
 
     }
-    
+    ChangeMode() {
+        if(this.changing)
+            return ;
+        this.changing = true;
+        mvc.apply();
+        console.log("changing")
+        this.getStatus().then( res =>{
+            res.state = !res.state;
+            console.log(res);
+            this.db.then(dbObject => dbObject.dbCreateOrUpdate("/settings", res, "sitemode").then(resp => {
+                this.mode = res.state;
+                this.siteMode = res;
+                this.changing = false;
+                mvc.apply();
+            }));
+        }) 
+    }
+    getStatus() {
+        return new Promise((resolve, rej) => {
+            this.db.then(dbObject => dbObject.dbGet("/settings", false, "sitemode").then(res => {
+                
+                console.log(res);
+                resolve(res);
+            }));
+        })
+    }
 }
