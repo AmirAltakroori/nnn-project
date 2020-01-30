0788f8b28e6938adec36f463973e21362586a994
Waleed Jubeh <wsameer70@gmail.com>
2020-01-30 22:06:35 +0300

    Get the state of the site

--- adminpanel/pages/homepage/js/home.js
+++ adminpanel/pages/homepage/js/home.js
@@ -1,6 +1,7 @@
   export class HomeController {
     constructor() {
-        this.Message = "hello";
+        this.mode = null;
+
         const userData = JSON.parse(sessionStorage.getItem('user'));
         if (!userData) {
             window.location.href = '/admin-panel-login/login.html';
@@ -8,5 +9,16 @@
         }
         this.userRole = userData.roleID;
         console.log(this.userRole)
+        this.db = dynamicImport("./js/backend.js");
+        console.log(this.db);
+        this.db.then(dbObject => dbObject.dbGet("/settings", false, "sitemode").then(res => {
+            this.mode = res.state;
+            console.log(res);
+            mvc.apply();
+        }));
+
+
+
     }
+    
 }
