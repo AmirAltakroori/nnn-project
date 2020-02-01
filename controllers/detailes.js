790299afb559d70a4ed3c3a26a44051631f9d378
QusaiHroub <qusaihroub.r@gmail.com>
2020-02-01 19:42:59 +0200

    get news from dataBase

--- controllers/detailes.js
+++ controllers/detailes.js
@@ -12,64 +12,51 @@
 import { DataBase } from "../services/DataBase.js";
 
 export class Details {
+
     constructor() {
-        // this.routeParams.newID;
-        this.newID = 0;
-        this.id = "";
-        this.title = "";
-        this.content = "";
-        this.categoryId = -1;
-        this.seoTitle = "";
-        this.seoTages = ""; // json string;
-        this.seoDescription = "";
-        this.isActive = false;
-        this.isMainNews = false;
-        this.isUrgentNews = false;
-        this.createDate = Date();
-        this._attachments = ""; // follwing couchDB structure.
 
         this.dataBase = new DataBase();
-        
-        this.relatedNews = [{
-            id: 1,
-            title: " 111لبنان.. دعوات لقطع الطرقات في اليوم الـ40 لانطلاق الاحتجاجات ",
-            img: "new2.png",
-            content: " اعلنت شركة أمازون الأميركية أنها تقاضي وزارة الدفاع (بنتاغون) بسبب استثنائها  من عطاء بقيمة عشرة مليارات دولار لتقديم ما يسمى خدمات الحوسبة السحابية   للوزارة ، وهو العطاء الذي مُنح لشركة مايكروسوفت111111",
-            path: "#"
-        }, {
-            id: 2,
-            title: "222 لبنان.. دعوات لقطع الطرقات في اليوم الـ40 لانطلاق الاحتجاجات ",
-            img: "new.jpg",
-            content: " اعلنت شركة أمازون الأميركية أنها تقاضي وزارة الدفاع (بنتاغون) بسبب استثنائها  من عطاء بقيمة عشرة مليارات دولار لتقديم ما يسمى خدمات   الحوسبة السحابية للوزارة، وهو العطاء الذي مُ نح لشركة مايكروسوفت222222 ",
-            path: "#"
 
-        }, {
-            id: 3,
-            title: " 333لبنان.. دعوات لقطع الطرقات في اليوم الـ40 لانطلاق الاحتجاجات ",
-            img: "firstNews.jpg",
-            content: " اعلنت شركة أمازون الأميركية أنها تقاضي وزارة الدفاع (بنتاغون) بسبب استثنائها  من عطاء بقيمة عشرة مليارات دولار لتقديم ما يسمى خدمات  الحوسبة السحابية للوزارة، وهو العطاء الذي مُ نح لشركة مايكروسوفت33333 ",
-            path: "#"
-        }, {
-            id: 4,
-            title: " لبنان.. دعوات لقطع الطرقات في اليوم الـ40 لانطلاق الاحتجاجات ",
-            img: "4.jpeg",
-            content: " اعلنت شركة أمازون الأميركية أنها تقاضي وزارة 44444الدفاع (بنتاغون) بسبب استثنائها  من عطاء بقيمة عشرة مليارات دولار لتقديم ما يسمى خدمات4444   الحوسبة السحابية للوزارة، وهو العطاء الذي مُ نح لشركة مايكروسوفت44444 444",
-            path: "#"
-        }, {
-            id: 5,
-            title: "55 لبنان.. دعوات لقطع الطرقات في اليوم الـ40 لانطلاق الاحتجاجات ",
-            img: "new.jpg",
-            content: " اعلنت شركة أمازون الأميركية أنها تقاضي وزارة الدفاع (بنتاغون) بسبب استثنائها  من عطاء بقيمة عشرة مليارات دولار لتقديم ما يسمى خدمات   الحوسبة السحابية  للوزارة، وهو العطاء الذي مُ نح لشركة مايكروسوفت 5555",
-            path: "#"
-        }, {
-            id: 6,
-            title: "66 لبنان.. دعوات لقطع الطرقات في اليوم الـ40 لانطلاق الاحتجاجات ",
-            img: "3.jpeg",
-            content: " اعلنت شركة أمازون الأميركية أنها تقاضي وزارة الدفاع (بنتاغون) بسبب استثنائها  من عطاء بقيمة عشرة مليارات دولار لتقديم ما يسمى خدمات   الحوسبة السحابية  للوزارة، وهو العطاء الذي مُ نح لشركة مايكروسوفت666 ",
-            path: "#"
-        }];
+        this.news;
 
+        this.relatedNews = [];
+        this.getNews();
 
+    }
+
+    getNews() {
+        this.dataBase.findByIndex("/news",
+                                ["_id", "content", "createDate", "title", "writerId", "isActive", "attachment", "categoryId"],"_id", mvc.routeParams.newID,
+                                'https://541e1dc0-354b-4134-ae7d-5eaa533a1bf9-bluemix.cloudant.com',
+                                 'Basic NTQxZTFkYzAtMzU0Yi00MTM0LWFlN2QtNWVhYTUzM2ExYmY5LWJsdWVtaXg6NDU2YjA3NzhjODFjOWNiMDk5NzZkODU1NjQ5MDM2YzRlYTE1MTQwZTk5NDNlNWM2MGE5ZDM1MGMwNDU5YzIwMw==')
+                                 .then( data => {
+                                     let iData = data;
+                                     this.news = iData.docs[0];
+                                     this.news.attachment = this.b64EncodeUnicode(this.news.attachment);
+                                     mvc.apply();
+                                     this.dataBase.findByIndex("/news",
+                                                             ["_id", "content", "title", "attachment", "categoryId"],"categoryId", this.news.categoryId,
+                                                             'https://541e1dc0-354b-4134-ae7d-5eaa533a1bf9-bluemix.cloudant.com',
+                                                              'Basic NTQxZTFkYzAtMzU0Yi00MTM0LWFlN2QtNWVhYTUzM2ExYmY5LWJsdWVtaXg6NDU2YjA3NzhjODFjOWNiMDk5NzZkODU1NjQ5MDM2YzRlYTE1MTQwZTk5NDNlNWM2MGE5ZDM1MGMwNDU5YzIwMw==')
+                                                              .then( data => {
+                                                                  let iData = data;
+                                                                  this.relatedNews = iData.docs.filter((el) => { return el._id != this.news._id});
+                                                                  for(news of this.relatedNews) {
+                                                                      news.attachment = this.b64EncodeUnicode(news.attachment);
+                                                                  }
+                                                                  mvc.apply();
+                                                              });
+                                 });
+    }
+
+    b64EncodeUnicode(utf8String) {
+        // first we use encodeURIComponent to get percent-encoded UTF-8,
+        // then we convert the percent encodings into raw bytes which
+        // can be fed into btoa.
+        return btoa(encodeURIComponent(utf8String).replace(/%([0-9A-F]{2})/g,
+            function toSolidBytes(match, p1) {
+                return String.fromCharCode('0x' + p1);
+        }));
     }
 
 }
