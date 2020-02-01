0e3a69088d70c9023897d4bd03060eee2838ecb6
SuhairShareef <suhairshareef1999@gmail.com>
2020-02-01 15:30:07 +0200

    Applying mvc

--- controllers/home.js
+++ controllers/home.js
@@ -10,8 +10,6 @@
      File description:
 */
 
-let categoryTitle;
-let categoryMainNews = [];
 
 export class Home {
     constructor() {
@@ -22,15 +20,21 @@ export class Home {
         }
 
         this.selectedNews = this.mainNews[0];
-
         this.slideIndex = 0;
         this.randomNews = this.getRandomNews();
-
-        this.categoriesList = getCategoriesList();
+        this.categoriesList = this.getCategoriesList();
 
         //this.showRandomNews(this.randomNews);
         //this.showmainNews ();
-        this.showCategoryNews();
+        
+        this.categoryMainNews = this.getCategoryMainNews();
+        if (this.categoryMainNews.lenght > 5) {
+          this.categoryMainNews = this.categoryMainNews.slice(0, 5);
+      };
+
+        this.firstCategoryMainNews = this.categoryMainNews[0];
+        this.categoryTitle = this.getCategoryTitle();
+
     }
 
     /*
@@ -227,73 +231,59 @@ export class Home {
 
         This function used to retrieve main news in category from database
     */
-    getCategoryNews () {
+    getCategoryMainNews () {
 
-    // I'll rewrite this function when DB was ready.
-        categoryTitle = [{
-                            title:"الأخبار العالمية",
-                            path:"#"
-                         }]
-
-        categoryMainNews = [{
+    // I'll rewrite this function when DB was ready.            
+        return [{
                             title: " القوة الخامسة للطبيعة.. اكتشاف قد يفك لغز المادة المظلمة",
                             path: "#",
                             authorName: "admin",
                             publishedDate: "12/12/2019",
-                            img:"img/img1.jpg",
-                            SubDescription:"فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم "
+                            img:"img1.jpg",
+                            SubDescription:"فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم ",
+                            id = 1
+                           },{
+                            title: " القوة الخامسة للطبيعة.. اكتشاف قد يفك لغز المادة المظلمة",
+                            path: "#",
+                            authorName: "admin",
+                            publishedDate: "12/12/2019",
+                            img:"img1.jpg",
+                            SubDescription:"فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم ",
+                            id = 2
+                           },{
+                            title: " القوة الخامسة للطبيعة.. اكتشاف قد يفك لغز المادة المظلمة",
+                            path: "#",
+                            authorName: "admin",
+                            publishedDate: "12/12/2019",
+                            img:"img1.jpg",
+                            SubDescription:"فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم ",
+                            id = 3
+                           },{
+                            title: " القوة الخامسة للطبيعة.. اكتشاف قد يفك لغز المادة المظلمة",
+                            path: "#",
+                            authorName: "admin",
+                            publishedDate: "12/12/2019",
+                            img:"img1.jpg",
+                            SubDescription:"فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم ",
+                            id = 4
+                           },{
+                            title: " القوة الخامسة للطبيعة.. اكتشاف قد يفك لغز المادة المظلمة",
+                            path: "#",
+                            authorName: "admin",
+                            publishedDate: "12/12/2019",
+                            img:"img1.jpg",
+                            SubDescription:"فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم فوز البرازيل بكأس العالم ",
+                            id = 5
                            }];
-
-
     }
 
-    /*
-        Show News.
+    getCategoryTitle () {
 
-        @tparam
-
-        @param
-
-        @returns
-
-        This function used to show the urgent news retrieved from database at Category Section in home bage
-    */
-    showCategoryNews () {
-        const categoryTitleDiv = document.getElementById('title');
-        const categoryMainNewsDiv = document.getElementById('main-news');
-        const categorySubNewsDiv = document.getElementById('sub-news');
-        this.getCategoryNews();
-        categoryTitleDiv.innerHTML=`<div class="category-header-name"><a class="link" href="${categoryTitle.title}"></a></div>
-                                    <div class="category-read-more "><a class="read-more-btn" href=${categoryTitle.path}> المزيد &gt;</a></div>`;
-
-
-
-        if(categoryMainNews.length > 0) {
-
-            categoryMainNewsDiv.innerHTML=`<div class="category-main-image-div">
-                                                <img id="category-image" src="${categoryMainNews[0].img}" alt="${categoryMainNews[0].title}">
-                                            </div>
-                                            <div class="category-main-title-and-date">
-                                                    <div class="category-main-title"><a class="link" href="${categoryMainNews[0].path}"></a></div>
-                                                    <div class="category-main-date">${categoryMainNews[0].publishedDate}</div>
-                                            </div>
-                                            <div class="category-main-details"></div>
-                                            <a class="read-more-btn category-btn" href=${categoryMainNews[0].path}>اقرأ المزيد &gt;</a>`;
-
-
-
-        categorySubNewsDiv.innerHTML="";
-
-        for (let i = 0; i < 3; i++) {
-
-            categorySubNewsDiv.innerHTML+=`<div class="category-content-random-item">
-                                                <img class="category-img-rnd" id="category-image" src="${categoryMainNews[0].img}" alt="">
-                                                <div class="category-randome-title"><a class="link" href="${categoryMainNews[0].path}">${categoryMainNews[0].title}</a> </div>
-                                            </div>`;
-
-        }
-    }
-
-    }
+      // I'll rewrite this function when DB was ready.
+          return {
+                              title:"الأخبار العالمية",
+                              id:1
+                           };
+      }
 
 }
