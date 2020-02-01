3c985d102a3e051637e1fa5b5c8315bfa52d77e0
diana mujahed <diana.muj98@gmail.com>
2020-02-01 15:59:42 +0200

    header with include is not working

---
+++ controllers/header.js
@@ -0,0 +1,51 @@
+
+    /*
+ *     NNN website.
+ *
+ *     This file is part of the NNN website.
+ *
+ *     Authors:
+ *     diana mujahed <diana.muj98@gmail.com>
+ *     Qusai Hroub <qusaihroub.r@gmail.com>
+       Aseel Arafeh <arafehaseel@gmail.com>
+ *
+ *     File description: this file contains the controller class of the website header which applies potato MVC framework
+ */
+
+export class Header {
+
+
+    constructor() {
+
+   this.categoriesList = [{
+    title: "الصفحة الرئيسية",
+    path: "/home"
+      }, {
+    title: "تكنولوجيا",
+    path: "/category/teachnology"
+      }, {
+    title: "علوم",
+    path: "/category/science"
+      }, {
+    title: "ثقافة",
+    path: "/category/knowledge"
+      }, { 
+    title: "اقتصاد",
+    path: "/category/economy"
+      }, {
+    title: "رياضة",
+    path: "/category/sport"
+      }, {
+    title: "فن",
+    path: "/category/art"
+      }, {
+    title: "سياسة",
+    path: "/category/politics"
+      }, {
+    title: "موسيقى",
+    path: "/category/music"
+      }];
+
+    
+}
+}
