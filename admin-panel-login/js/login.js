277d20f21fddb5bd3bce110d9c7dc3b6b968823a
unknown <hafezdema1999@gmail.com>
2020-02-06 14:29:56 +0200

    fix login

--- admin-panel-login/js/login.js
+++ admin-panel-login/js/login.js
@@ -66,7 +66,7 @@ function verification() {
     // let user = getUser(username.value, password.value);
     let currentUser = null;
     dynamicImport("./../../adminpanel/js/backend.js").then(db => {
-    db.dbGet("/users/",false,username.value).then( user => {
+    db.dbGet("/users",false,username.value).then( user => {
         currentUser = user;
         let text = "";
         color = "#ffffff"
